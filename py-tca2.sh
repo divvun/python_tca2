4dfcbf0c35128bf5a9d48b19ca3033484951d3fe
Børre Gaup <albbas@gmail.com>
2024-01-04 14:33:43 +0100

    Fiks stier

--- py-tca2.sh
+++ py-tca2.sh
@@ -10,5 +10,5 @@ java \
     -jar /Users/bga001/repos/giellalt/CorpusTools/corpustools/tca2/dist/lib/alignment.jar \
     -cli-plain \
     -anchor=data/anchor-nob-fkv.txt \
-    -in1=data/kommisjonen_21.08.2020_nob.txt_nob.sent \
-    -in2=data/kommisjonen_21.08.2020_fkv.txt_fkv.sent
+    -in1=data/nob.sent \
+    -in2=data/fkv.sent
