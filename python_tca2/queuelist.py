a4c162c3e19a4f5cc67f9eb4f6d1ab8895bfbfc6
Børre Gaup <albbas@gmail.com>
2024-05-02 22:28:11 +0200

    Make the remove function manageble

--- python_tca2/queuelist.py
+++ python_tca2/queuelist.py
@@ -19,40 +19,26 @@ class QueueList:
         return queue_entry in self.entries
 
     def remove(self, pos: List[int]):
-        t = 0
         for queue_entry in self.entries:
-            hit = False
-            current = list(queue_entry.path.position)
-            current_ix = len(queue_entry.path.steps) - 1
-            done = False
-            while not done:
-                hope = True
-                for t in range(constants.NUM_FILES):
-                    if current[t] < pos[t]:
-                        hope = False
-                        break
-                if hope:
-                    eq = True
-                    for t in range(constants.NUM_FILES):
-                        if current[t] != pos[t]:
-                            eq = False
-                            break
-                    if eq:
-                        hit = True
-                        done = True
-                    elif current_ix >= 0:
-                        for t in range(constants.NUM_FILES):
-                            current[t] -= queue_entry.path.steps[current_ix].increment[
-                                t
-                            ]
-                        current_ix -= 1
-                    else:
-                        done = True
-                else:
-                    done = True
-
-            if hit:
+            if self.is_hit(pos, queue_entry):
                 queue_entry.remove()
+
+    def is_hit(self, pos, queue_entry):
+        current = list(queue_entry.path.position)
+        current_ix = len(queue_entry.path.steps) - 1
+
+        while current_ix > -1:
+            if not all(current[t] >= pos[t] for t in range(constants.NUM_FILES)):
+                return False
+
+            if all(current[t] == pos[t] for t in range(constants.NUM_FILES)):
+                return True
+
+            for t in range(constants.NUM_FILES):
+                current[t] -= queue_entry.path.steps[current_ix].increment[t]
+            current_ix -= 1
+
+        return False
 
     def remove_for_real(self):
         to_remove = [queue_entry for queue_entry in self.entries if queue_entry.removed]
