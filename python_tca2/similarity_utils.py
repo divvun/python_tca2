3226a071cb4496e83c67c0576de15ea7c5121307
Børre Gaup <albbas@gmail.com>
2025-04-09 23:29:03 +0200

    Simplify the tests somewhat

--- python_tca2/similarity_utils.py
+++ python_tca2/similarity_utils.py
@@ -249,27 +249,22 @@ def adjust_for_length_correlation(  # noqa: PLR0913
     Returns:
         The adjusted similarity score based on the correlation analysis.
     """
-    new_score = 0.0
     lower_limit = 0.4
     upper_limit = 1.0
     length_correlation_factor = calculate_length_correlation_factor(
         length1, length2, ratio
     )
 
-    if element_count1 > 0 and element_count2 > 0 and element_count1 != element_count2:
-        if length_correlation_factor < lower_limit / 2:
-            new_score = score + 2
-        elif length_correlation_factor < lower_limit:
-            new_score = score + 1
-        else:
-            new_score = score
-    elif length_correlation_factor < lower_limit / 2:
-        new_score = score + 2
-    elif length_correlation_factor < lower_limit:
-        new_score = score + 1
-    elif length_correlation_factor > upper_limit:
-        new_score = score / 3
-    else:
-        new_score = score
+    if length_correlation_factor < lower_limit / 2:
+        return score + 2
 
-    return new_score
+    if length_correlation_factor < lower_limit:
+        return score + 1
+
+    if length_correlation_factor > upper_limit and not (
+        element_count1 > 0 and element_count2 > 0 and element_count1 != element_count2
+    ):
+        return score / 3
+
+    # if length_correlation_factor is between lower_limit and upper_limit
+    return score
