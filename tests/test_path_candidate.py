a6d12e89146a90666e8d943d15fbcaa4d5978d74
Børre Gaup <albbas@gmail.com>
2025-07-13 01:08:51 +0200

    Give correct arguments to test

--- tests/test_path_candidate.py
+++ tests/test_path_candidate.py
@@ -4,12 +4,12 @@ from python_tca2.path_candidate import PathCandidate
 
 def test_is_hit():
     path_candidate = PathCandidate(
-        position=[1, 1],
+        position=(1, 1),
         alignment_suggestions=[
-            AlignmentSuggestion([1, 0]),
-            AlignmentSuggestion([0, 1]),
+            AlignmentSuggestion((1, 0)),
+            AlignmentSuggestion((0, 1)),
         ],
     )
 
-    assert path_candidate.has_hit([1, 1])
-    assert not path_candidate.has_hit([1, 2])
+    assert path_candidate.has_hit((1, 1))
+    assert not path_candidate.has_hit((1, 2))
