5e624361e94c49e82b4bea372889beed077aa89c
Børre Gaup <albbas@gmail.com>
2025-07-14 17:56:44 +0200

    Try to smoothen the code

--- python_tca2/alignmentmodel.py
+++ python_tca2/alignmentmodel.py
@@ -172,25 +172,25 @@ class AlignmentModel:
         path_candidates = [PathCandidate(position=start_position)]
         for _ in range(self.max_path_length):
             next_path_candidates: list[PathCandidate] = []
-            for path_candidate in path_candidates:
-                if not path_candidate.removed:
-                    if not path_candidate.end:
-                        for new_path_candidate in self.extend_current_path(
-                            path_candidate,
-                            best_path_scores=best_path_scores,
-                        ):
-                            if new_path_candidate is not None:
-                                if not new_path_candidate.end:
-                                    pos = new_path_candidate.position
-                                    for p in path_candidates:
-                                        if p.has_hit(pos):
-                                            p.removed = True
-                                    for npc in next_path_candidates:
-                                        if npc.has_hit(pos):
-                                            npc.removed = True
-                                    next_path_candidates.append(new_path_candidate)
-                                elif new_path_candidate not in next_path_candidates:
-                                    next_path_candidates.append(new_path_candidate)
+            for path_candidate in [
+                pc for pc in path_candidates if not (pc.removed or pc.end)
+            ]:
+                for new_path_candidate in self.extend_current_path(
+                    path_candidate,
+                    best_path_scores=best_path_scores,
+                ):
+                    if new_path_candidate is not None:
+                        if not new_path_candidate.end:
+                            pos = new_path_candidate.position
+                            for p in path_candidates:
+                                if p.has_hit(pos):
+                                    p.removed = True
+                            for npc in next_path_candidates:
+                                if npc.has_hit(pos):
+                                    npc.removed = True
+                            next_path_candidates.append(new_path_candidate)
+                        elif new_path_candidate not in next_path_candidates:
+                            next_path_candidates.append(new_path_candidate)
 
             if not next_path_candidates:
                 return path_candidates
